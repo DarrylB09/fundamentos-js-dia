73b614d8ad2de8bde72fbc96b42f283e86e6de39
DarrylB09 <darryldayara2006@gmail.com>
2024-11-07 17:41:45 -0500

    Se crea el proyecto con los ejemplos de los fundamentos de JavaScript

---
+++ FundamentosJS/recursos/js/Main.js
@@ -0,0 +1,41 @@
+/*Metodo mas usado*/
+//document.getElementById("demo").innerHTML = "Hello JavaScript";
+//ocument.getElementById("demo").style.fontSize = "35px";
+
+/*Salidas de datos */
+//window.alert(5+2);
+//console.log(6+2);
+
+/* Declaracion*/
+let a,b,c;
+a = 5;
+b = 6;
+c = a + b;
+
+//window.alert(c)
+//document.getElementById("demo").innerHTML = c;
+
+/*Variables */
+let p, q, r;
+p=5;
+q= 7;
+const m = 8;
+r= p + q +m;
+document.getElementById("demo1").innerHTML = r;
+
+/*Tipos de datos  */
+let num = 24;  //Numero entero
+let name = "Darryl Bolaños";  //Cadena
+let km = 14.5;  //Numero decimal
+let isColombia = true;  //Booleano
+let obj = {firsname: "Ana", lastname: "Perez"}; //Objeto
+
+let y = 17 + "POO";
+let unir = "17" + "POO";
+window.alert(unir);
+window.alert(y);
+
+//Declaracion de una funcion 
+function myFuntion(){
+    document.getElementById("demo2").innerHTML = "New paragraph";
+}
